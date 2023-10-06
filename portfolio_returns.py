1183debf81c8e7749288bc97739364731043a1d1
Priya Patel <patelpriya19961996@gmail.com>
2023-10-06 17:50:12 -0400

    Adding function return_over_time_period

--- portfolio_returns.py
+++ portfolio_returns.py
@@ -19,3 +19,55 @@ def calculate_end_date(start_date, years = 30):
     '''
     end_date = start_date + relativedelta(years = years)
     return end_date
+
+
+# Function calculating return over time period
+def return_over_time_period(df, start_date, end_date, summer = True):
+    '''Function return_over_time_period calculates return over given time period, time weighted rate of return as well as sharpe. 
+    
+    The time period is selected based on given input parameters start_date and end_date and makes an investment for all months or excluding summer moths based on the parameter summer. If summer parameter is True we invest in all the months and if not then exclude summer months. 
+    
+    The default value for Summer parameter is set to True. 
+    The function returns time_weighted_return,total return over a given time period and sharpe ratio.'''
+    
+    required_df = df[(df['date_ff_factors'] >= start_date) & (df['date_ff_factors'] < end_date)].reset_index(drop = True)
+    # Adding a column which shows $1000 investment monthly or $1333.33 if excluding summer months
+    if summer:
+        required_df['Investment_Amt'] = 1000
+    else:
+        summer_months = [6,7,8]
+        for i in range(len(required_df)):
+            if required_df.loc[i, 'date_ff_factors'].month in summer_months:
+                required_df.loc[i,'Investment_Amt'] = 0
+            else:
+                required_df.loc[i,'Investment_Amt'] = 1333.33
+
+    # Adding a column to show cummilative sum of investment
+    required_df['Monthly_Amt_Aggregate'] = required_df['Investment_Amt'].cumsum()
+
+    # Intially setting monthly return 0 for all months
+    required_df['Monthly_Return'] = 0
+    required_df['Holding_Period_Return'] = 0
+
+    # Calculating monthly return
+    for i in range(len(required_df)):
+        if i == 0:
+            required_df.loc[i, 'Monthly_Return'] = required_df.loc[i, 'Investment_Amt']*(1 + required_df.iloc[i,1] + required_df.iloc[i,4])
+            if required_df.loc[i, 'Investment_Amt'] == 0:
+                required_df.loc[i, 'Holding_Period_Return'] = 0
+            else:
+                required_df.loc[i, 'Holding_Period_Return'] = (required_df.loc[i, 'Monthly_Return'] - required_df.loc[i, 'Investment_Amt'])/required_df.loc[i,'Investment_Amt']
+        else:
+            required_df.loc[i, 'Monthly_Return'] = (required_df.loc[i, 'Investment_Amt']+required_df.loc[i-1, 'Monthly_Return'])*(1 + required_df.iloc[i,1] + required_df.iloc[i,4])
+            
+            numerator = (required_df.loc[i, 'Monthly_Return'] - (required_df.loc[i,'Investment_Amt'] + required_df.loc[i-1,'Monthly_Return']))
+            denominator = (required_df.loc[i,'Investment_Amt'] + required_df.loc[i-1,'Monthly_Return'])
+            if denominator == 0:
+                required_df.loc[i, 'Holding_Period_Return'] = 0
+            else:
+                required_df.loc[i, 'Holding_Period_Return'] = numerator/denominator
+                
+    time_weighted_return = np.prod(required_df['Holding_Period_Return'].values + 1)
+    sharpe_ratio = required_df['Mkt-RF'].mean()/required_df['Mkt-RF'].std()
+
+    return round(time_weighted_return,3), round(required_df.iloc[-1,-2], 3), round(sharpe_ratio, 3)
